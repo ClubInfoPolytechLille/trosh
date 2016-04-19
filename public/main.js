5186f0a6c3e909f0e02a2e26d9915e134b9e7dfc
Geoffrey Frogeye <geoffrey@frogeye.fr>
2016-04-19 23:56:20 +0200

    Best score

--- public/main.js
+++ public/main.js
@@ -1,3 +1,12 @@
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
 $(function() {
     var socket = io();
     var scores = [];
@@ -19,6 +28,8 @@ $(function() {
         $('#sub').text(msg);
     });
 
+    var oldScore = 0;
+
     function redrawScores() {
         var sortable = [];
         scores.sort(function(b, a) {return a.score - b.score})
@@ -28,6 +39,15 @@ $(function() {
             if (i < 15) {
                 var obj = scores[i];
                 list.append($('<li>').text(obj.score + ' - ' + obj.name));
+            }
+        }
+        if (scores[0]) {
+            $('#bestBlock').css('display', 'block');
+            $('#bestBlock .score').text(scores[0].score);
+            $('#bestBlock .name').text(scores[0].name);
+            if (scores[0].score > oldScore) {
+                $('#bestBlock').animateCss('rotateIn');
+                oldScore = scores[0].score;
             }
         }
     }
@@ -50,6 +70,7 @@ $(function() {
         var scale = (Math.sin(now*5) + 1)/5 + 0.7;
         var rot = Math.sin(now*10)/10;
         logo.css('transform', 'rotate(' + rot + 'rad) scale(' + scale + ')');
+        rainbowColor($('#bestBlock .score'));
         requestAnimationFrame(anim);
 
     })();
