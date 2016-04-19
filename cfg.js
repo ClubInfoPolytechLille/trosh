71899fcf151d161c535aa3136997a45228b70359
Geoffrey Frogeye <geoffrey@frogeye.fr>
2016-04-19 06:23:49 +0200

    Récupération du script du mur de tweets

---
+++ cfg.js
@@ -0,0 +1,8 @@
+module.exports = {
+	consumer_key: '0jLhI21FclBdw7ArkzBIPj4W6',
+	consumer_secret: 'Q805J8iKRuhGKWnYDiB8gL6u9LnRFUrJFGztLMpFALrLMEX0eg',
+	token: '527372302-oQneyUhrb7aWRAJldyFN535bql3kw9fS7nai7LNy',
+	access_token_key: '527372302-oQneyUhrb7aWRAJldyFN535bql3kw9fS7nai7LNy',
+	token_secret: '7FQ3w5jzlePCEkrJ4qt3hPvcHXB8vstaBKCubzGdWLV0Z',
+	access_token_secret: '7FQ3w5jzlePCEkrJ4qt3hPvcHXB8vstaBKCubzGdWLV0Z'
+}
